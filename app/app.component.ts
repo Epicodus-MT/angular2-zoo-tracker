cd4f804a7894e54bfac541614f04f9cf73971d4a
Mara Timberlake <maratimberlake@msn.com>
2017-08-18 10:36:38 -0700

    setup skeleton of project files

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,12 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  <div class="container">
-    <h1>Zoo Tracker</h1>
+   <div class="container">
+      <h1>Zoo Tracker</h1>
+      <h3>{{currentSpecies}}</h3>
   </div>
   `
 })
 
 export class AppComponent {
-
+  currentSpecies: string = 'Tapir';
 }
